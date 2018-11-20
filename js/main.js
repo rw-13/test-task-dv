8524608a543d83278365fc3814f60dfe853ebb0f
rw-13 <generated.in@gmail.com>
2018-11-20 23:11:30 +0300

    calculate position elements

--- js/main.js
+++ js/main.js
@@ -4,18 +4,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	let data = JSON.parse(loadedData);
 	let rootContainer = document.getElementById("root");
 
-	// function Configurator() {
-	// 	this._height = 0;
-	// 	this._width = rootContainer.clientWidth;
-	// 	this._columns = 3;
-	// 	this._gutterX = 10;
-	// 	this._gutterY = 10;
-
-	// 	this.cartWidth = function() {
-	// 		return ((this._width-(this._columns-1)*this._gutterY)/this._columns);
-	// 	};
-	// }
-
 	// Конструктор контейнера с карточками
 	function CartContainer(data) {
 		this._data = data;
@@ -33,9 +21,9 @@ document.addEventListener('DOMContentLoaded', function(){
 		this.createEl = function() {
 			// Вырезали случ.объект из this._data
 			let dataCart = this.getRandomEl();
-			let node = new Cart(dataCart);
-			console.log(node);
-			rootContainer.appendChild(node._el);
+			return new Cart(dataCart);
+			
+			// rootContainer.appendChild(node._el);
 		};
 	}
 
@@ -45,18 +33,68 @@ document.addEventListener('DOMContentLoaded', function(){
 		let theme = (props.theme) ? props.theme : '';
 		let originalText = (props.sourceText) ? props.sourceText : '';
 		let transtatedText = (props.translation) ? props.translation : '';
-		
-		this._el = document.createElement('div');
-		this._el.innerHTML = '<div class="cart__title" data-translate="' + transtatedText +'">' + 
+		let el = document.createElement('div');
+		el.innerHTML = '<div class="cart__title" data-translate="' + transtatedText +'">' + 
 				theme + '</div>' + '<div class="cart__content">' + originalText + '</div>';
-		this._el.classList.add('cart');
+		el.classList.add('cart');
+		return el;
 	}
 
 	// -------------------------BOOTSTRAP-----------------------------
+	// Контейнер
+	// Кол-во объектов из JSON
+	// Массив элементов
 	let obj = new CartContainer(data);
 	let countElem = obj.getData().length;
+	let elemArray = [];
+
+	// Кол-во колонок
+	// Отступ между колонками
+	// Ширина контейнера
+	// Высота карточки
+	// Ширина карточки
+	let columns = 3;
+	let gutter = 10;
+	let width = rootContainer.clientWidth;
+	let cartHeight = 0;
+	let cartWidth = (width-(columns-1)*gutter)/columns;
+
 	for (let i=0; i<countElem; i++) {
-		obj.createEl();
+		let node = obj.createEl();
+
+		// Смещение по горизонтали
+		let left = (i%columns) ? ((i%columns)*(gutter+cartWidth)) : 0;
+		node.style.left = left + 'px';
+		node.style.width = cartWidth + 'px';
+
+		rootContainer.appendChild(node);
+
+		// Смещение по вертикали
+		let top = getTop(elemArray, i);
+		node.style.top = top + 'px';
+
+		elemArray.push(node);
 	}
 
+	// Работа с массивом элементов cart
+	function getTop(array, pos) {
+		// Смещение карточки от верхней границы контейнера
+		// Колонка и строка в которой находится карточка
+		let offsetTop = 0;
+		let col = pos%columns;
+		let row = Math.floor(pos/columns);
+
+		// Первая строка с нулевым отступом
+		if (row === 0) {
+			return offsetTop;
+		}
+		// Бегаем по строкам
+		for(let i=0; i<row; i++) {
+			let k = i*columns+col;
+			if (array[k]) {
+				offsetTop += (array[k].clientHeight + gutter);
+			}
+		}
+		return offsetTop;
+	}
 });
